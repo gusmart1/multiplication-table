c450e2487c600b3bfc57b5c41e4db23f77b47974
gmartinson <mart6360@umn.edu>
2025-10-13 14:43:54 -0500

    works in flex

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function getInputValueAsNumber(inputID ) {
 function createWithHtmlTable(rows, cols, highlight) { // method declaration, has two parameters
     let string // define a variable called string
     if (rows && cols) { // if rows and columns were provided
-        string = "<table>\n" // start building the HTML table
+        string = "<div class='table'>\n" // start building the HTML table
 
         // first "for" loop to build the rows of the table
         for (let row = 1; row <= rows; row++) {
@@ -18,29 +18,29 @@ function createWithHtmlTable(rows, cols, highlight) { // method declaration, has
             if (highlight && highlight === row) {
                 // if highlight "IS TRUTHY" and its value matches the row
                 // we're on, then do this next
-                string += "<tr class='highlight'>\n"
+                string += "<div class='highlight'>\n"
             } else {
                 // otherwise just start a regular HTML row with no class
-                string += "<tr>\n" // start building an HTML row
+                string += "<div class='cell'>\n" // start building an HTML row
             }
 
             // second "for" loop to build the columns of EACH row
             for (let col = 1; col <= cols; col++) {
 
                 if (highlight && highlight === col) {
-                    string += "<td class='highlight'>\n"
+                    string += "<div class='highlight'>\n"
                 } else {
-                    string += "<td>" // start building a single "cell" or column in the HTML row
+                    string += "<div class='cell'>" // start building a single "cell" or column in the HTML row
                 }
 
 
                 string += row * col // populate the contents of the HTML cell
-                string += "</td>\n" // complete the HTML cell
+                string += "</div>\n" // complete the HTML cell
             }
-            string += "</tr>\n" // complete the HTML row
+            string += "</div>\n" // complete the HTML row
         }
 
-        string += "</table>\n" // complete the HTML table
+        string += "</div>\n" // complete the HTML table
     } else { // I guess the method wasn't provided either rows or columns
         string = 'Provide some inputs' // set value of "string" variable to "Provide some inputs"
     }
