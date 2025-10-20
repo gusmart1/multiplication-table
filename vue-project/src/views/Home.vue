dc51f85c9a7ac9a166990295f9fb9400addcd840
gmartinson <mart6360@umn.edu>
2025-10-20 16:54:24 -0500

    starting vue stuff

---
+++ vue-project/src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Hello World!</h1>
+  <p>Welcome to the ingress page of my website.</p>
+</template>
+
+<style scoped></style>
---
+++ vue-project/src/views/MultiplicationTable.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+
+import { ref } from 'vue'
+
+let rows = ref(10)
+let cols = ref(10)
+let name = ref(null)
+</script>
+
+<template>
+  <h1>Enter a value:</h1>
+
+  <div class="inputs">
+    <label id="name">
+      <span>What's your name:</span>
+      <input  id="name" v-model="names"/>
+    </label>
+
+    <label id="rows">
+      <span>Rows:</span>
+      <input type="number" id="rows" v-model.number="rows"/>
+    </label>
+
+    <label id="cols">
+      <span>Cols:</span>
+      <input type="number" id="cols" v-model.number="cols"/>
+    </label>
+
+    <p>Hi there {{ name }}, lets build our table </p>
+
+    <p v-if="name">HI there</p>
+    <p v-else>Hi there {{ name }}, lets build our table: </p>
+
+    <p>How many  {{ rows }}</p>
+    <p>How many  {{ cols }}</p>
+
+  </div>
+</template>
+
+<style scoped>
+div.inputs {
+  border: 1px solid black;
+  width: 50%;
+  margin: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  label {
+    padding: 10px;
+    border: 1px solid black;
+    display: flex;
+    gap: 15px;
+  }
+}
+</style>
